adebb7a8ee17c243e29d56847b9a9628e0955279
ckhatri <chirag.k.khatri@gmail.com>
2016-01-20 17:25:21 -0600

    UPDATING ISH

---
+++ ex5.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(int argc, char *argv[]) {
+    int distance = 100;
+
+    //comment
+    /*
+     * comment
+     */
+    printf("You are %d miles away.\n", distance);
+    return 0;
+}
+
+
